a37f0df9ff829c5bdaaa10dd6af46ad89c1adc62
Julita <bilajulia998@gmail.com>
2020-09-15 14:46:10 +0300

    add some information about TOPPING

--- script.js
+++ script.js
@@ -2,19 +2,44 @@ function Hamburger(){
 
 }
 
-Hamburder.SIZE_SMALL = {
+Hamburger.SIZE_SMALL = {
     price: 50,
     calories: 20
 }
 
-Hamburder.SIZE_MIDDLE = {
+Hamburger.SIZE_MIDDLE = {
     price: 75,
     calories: 30
 }
 
-Hamburder.SIZE_BIG = {
+Hamburger.SIZE_BIG = {
     price: 100,
     calories: 40
+}
+
+Hamburger.TOPPING_CHEESE = {
+    price = 10,
+    calories = 20
+}
+
+Hamburger.TOPPING_SALAD = {
+    price = 20,
+    calories = 5
+}
+
+Hamburger.TOPPING_POTATO = {
+    price = 15,
+    calories = 10
+}
+
+Hamburger.TOPPING_SAUCE = {
+    price = 15,
+    calories = 0
+}
+
+Hamburger.TOPPING_MAYO = {
+    price = 20,
+    calories = 5
 }
 
 const hamburger = new Hamburger(Hamburger.SIZE_SMALL);
