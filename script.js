cc66a14e51c17bcc940b03d06ddae64ca5a6cadb
Julita <bilajulia998@gmail.com>
2020-09-15 16:16:40 +0300

    finish

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-function Hamburger(){
-
+function Hamburger(size) {
+    this.size = size;
+    this.toppings = [];
 }
 
 Hamburger.SIZE_SMALL = {
@@ -18,34 +19,50 @@ Hamburger.SIZE_BIG = {
 }
 
 Hamburger.TOPPING_CHEESE = {
-    price = 10,
-    calories = 20
+    price: 10,
+    calories: 20
 }
 
 Hamburger.TOPPING_SALAD = {
-    price = 20,
-    calories = 5
+    price: 20,
+    calories: 5
 }
 
 Hamburger.TOPPING_POTATO = {
-    price = 15,
-    calories = 10
+    price: 15,
+    calories: 10
 }
 
 Hamburger.TOPPING_SAUCE = {
-    price = 15,
-    calories = 0
+    price: 15,
+    calories: 0
 }
 
 Hamburger.TOPPING_MAYO = {
-    price = 20,
-    calories = 5
+    price: 20,
+    calories: 5
 }
 
-const hamburger = new Hamburger(Hamburger.SIZE_SMALL);
-// добавка из майонеза
-hamburger.addTopping(Hamburger.TOPPING_MAYO);
+Hamburger.prototype.addTopping = function(topping) {
+    this.toppings.push(topping);
+}
+
+Hamburger.prototype.getCallories = function() {
+    return this.toppings.reduce(function (sum, topping) {
+        return sum + topping.calories;
+    }, this.size.price);
+}
+
+Hamburger.prototype.getPrice = function() {
+    return this.toppings.reduce(function(sum, topping) {
+        return sum + topping.price;
+    }, this.size.price);
+}
+
+const hamburger = new Hamburger(Hamburger.SIZE_MIDDLE);
+
+hamburger.addTopping(Hamburger.TOPPING_SAUCE);
 hamburger.addTopping(Hamburger.TOPPING_POTATO);
 
-console.log(`Price with sauce: “ + ${hamburger.getPrice()}`);
-console.log(`Callories with sauce: “ + ${hamburger.getCallories()}`);
+console.log(`Price with sauce: ${hamburger.getPrice()}`); // 105
+console.log(`Callories with sauce: ${hamburger.getCallories()}`); // 85
