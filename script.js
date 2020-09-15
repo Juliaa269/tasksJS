a43cec7bbd88147689d5b92f52e3860d1538ea0e
Julita <bilajulia998@gmail.com>
2020-09-15 14:41:19 +0300

    add some information about sizes

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ function Hamburger(){
 
 }
 
+Hamburder.SIZE_SMALL = {
+    price: 50,
+    calories: 20
+}
+
+Hamburder.SIZE_MIDDLE = {
+    price: 75,
+    calories: 30
+}
+
+Hamburder.SIZE_BIG = {
+    price: 100,
+    calories: 40
+}
+
 const hamburger = new Hamburger(Hamburger.SIZE_SMALL);
 // добавка из майонеза
 hamburger.addTopping(Hamburger.TOPPING_MAYO);
